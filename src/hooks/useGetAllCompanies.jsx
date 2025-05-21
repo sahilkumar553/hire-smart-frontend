8a022ceef065e636ec19246dfb456b280964e2b0
Sahil Kumar Gupta <sahilkumargupta553@gmail.com>
2025-05-21 12:22:27 +0530

    update

--- src/hooks/useGetAllCompanies.jsx
+++ src/hooks/useGetAllCompanies.jsx
@@ -1,6 +1,6 @@
 import { setCompanies} from '@/redux/companySlice'
 import { COMPANY_API_END_POINT} from '@/utils/constant'
-import axios from 'axios'
+import api from '@/utils/axios'
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 
@@ -9,17 +9,17 @@ const useGetAllCompanies = () => {
     useEffect(()=>{
         const fetchCompanies = async () => {
             try {
-                const res = await axios.get(`${COMPANY_API_END_POINT}/get`,{withCredentials:true});
-                console.log('called');
+                const res = await api.get(`${COMPANY_API_END_POINT}/get`);
+                console.log('Companies API called');
                 if(res.data.success){
                     dispatch(setCompanies(res.data.companies));
                 }
             } catch (error) {
-                console.log(error);
+                console.log('Error fetching companies:', error);
             }
         }
         fetchCompanies();
-    },[])
+    },[dispatch])
 }
 
 export default useGetAllCompanies
